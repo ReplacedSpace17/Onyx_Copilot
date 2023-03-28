d90cbfd0eb79cf7220164a558d84334f8c8f56fa
Javier Gutierrez <replacedspace17@gmail.com>
2023-03-28 00:30:52 -0600

    Creacion del componente menu, header y configurarion de las alertas para el Home

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
+import Swal from 'sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from './Header';
 import Menu from './Menu';
 
 
 function App() {
+  useEffect(() => {
+    Swal.fire({
+      title: '¡Bienvenid@!',
+      text: 'Te damos la bienvenida a Onyx Copilot, una plataforma que te ayudará a estimar precios sobre servicios relacionados al 3D Printing. ',
+      icon: 'info',
+      confirmButtonText: '¡Vamos a explorar!'
+    });
+  }, []);
+
+
   return (
-    <div>
+    <div >
       <Header />
       <Menu />
     </div>
--- src/Menu.js
+++ src/Menu.js
@@ -1,23 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaHome, FaUser, FaCog } from 'react-icons/fa';
+import icon1 from './images/service1_ico.png';
+import icon2 from './images/service2_ico.png';
+import icon3 from './images/service3_ico.png';
+import 'typeface-roboto';
+import 'typeface-roboto-mono';
+import Swal from 'sweetalert2';
+
+
+
+import "./EstilosHome.css";
 
 function Menu() {
+  const [h1Value, setH1Value] = useState('Bienvenidos/as');/*Valor inicial del H1*/
+  const [value, setValue] = useState('Bienvenidos/as');/*Valor inicial del H1*/
   return (
-    <div style={{ display: 'flex', justifyContent: 'center' }}>
-      <div style={{ backgroundColor: '#e1e1e1', padding: '1rem', margin: '1rem', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-        <FaHome style={{ marginRight: '0.5rem' }} />
-        <a href="Cotizador_impresion/impresion.js">Option 1</a>
+
+  <div className='divFondo'>
+    <h1 id='titleHome'>{h1Value}</h1>
+
+    <div style={{ display: 'flex', justifyContent: 'center', backgroundColor: '#e0e0e0', paddingTop: '0px'}}>
+
+      
+      <div className="cards" onMouseEnter={() => setH1Value('Cotizar impresión 3D')} >
+          <img src={icon1} alt="logo" style={{ height: '100px'}} />
+          <h2 className="labelCards">Impresión 3D</h2>
       </div>
-      <div style={{ backgroundColor: '#e1e1e1', padding: '1rem', margin: '1rem', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-        <FaUser style={{ marginRight: '0.5rem' }} />
-        <a href="#">Option 2</a>
+      <div className="cards" onMouseEnter={() => setH1Value('Diseña e imprime')} >
+          <img src={icon2} alt="logo" style={{ height: '100px'}} />
+          <h2 className="labelCards">Diseño e impresión 3D</h2>
       </div>
-      <div style={{ backgroundColor: '#e1e1e1', padding: '1rem', margin: '1rem', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-        <FaCog style={{ marginRight: '0.5rem' }} />
-        <a href="#">Option 3</a>
+      <div className="cards"onMouseEnter={() => setH1Value('Imprime por lotes')} >
+          <img src={icon3} alt="logo" style={{ height: '100px'}} />
+          <h2 className="labelCards">Impresión por lotes</h2>
       </div>
+
+      
     </div>
+    <button class="btnAyuda" onClick={mostrarAyuda}>
+    <span>¿Cual debería elegir?</span>
+    </button>
+  </div>
   );
+}
+
+
+function mostrarAyuda() {
+  Swal.fire({
+    title: 'Mensaje de alerta',
+    html: '<span style="color: red; font-size: 24px;">Este es un mensaje de alerta en rojo y con letra grande.</span>',
+    icon: 'warning',
+    confirmButtonText: 'Aceptar'
+  });
+  
+  
 }
 
 export default Menu;
